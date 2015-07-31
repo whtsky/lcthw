c14ff505f30fe7801ec751c3b3f496e96b5c423f
whtsky <whtsky@gmail.com>
2015-07-31 23:41:48 +0800

    ex20-ex22

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CFLAGS=-Wall -g
 
-all: ex19
-
 ex19: object.o
+
+ex22_main: ex22.o
---
+++ ex22.h
@@ -0,0 +1,16 @@
+#ifndef LCTHW_EX22_H
+#define LCTHW_EX22_H
+
+// make THE SIZE in ex22.c available to other .c files
+extern int THE_SIZE;
+
+// gets and sets an internal static variable in ex22.c
+int get_age();
+void set_age(int age);
+
+// updates a static variable that's inside update_ratio
+double update_ratio(double ratio);
+
+void print_size();
+
+#endif //LCTHW_EX22_H
